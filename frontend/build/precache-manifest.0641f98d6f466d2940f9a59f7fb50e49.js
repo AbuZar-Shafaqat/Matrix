9f68a69007b75bd177514f3b5d0372fd4a84737e
AbuZar-Shafaqat <abuzarshafaqat26@gmail.com>
2022-12-20 11:56:51 +0500

    Deploy project on live server

---
+++ frontend/build/precache-manifest.0641f98d6f466d2940f9a59f7fb50e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df106663a63e14d60e33a068d8ce3ba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e22595797053f4bd8b8",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e22595797053f4bd8b8",
+    "url": "/static/js/main.56b8316c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
